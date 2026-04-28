19a1a15da27049fd7e25604068007cf9a573d1ea
solamame0210 <solamame.0210@gmail.com>
2026-04-29 03:32:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def draw_symbol(s):
     position:absolute;
     top:50%; left:50%;
     transform:translate(-45%,-47%) rotate(90deg);
-    font-size:130px;">
+    font-size:120px;">
     ＊
   </div>
 """
